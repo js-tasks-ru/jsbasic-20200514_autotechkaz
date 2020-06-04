ea8f530795b6efcfff09cc4b947f9ecb6c4ca5bc
Mikhail_M <autotechkaz@gmail.com>
2020-06-04 23:45:24 +0600

    4 задание

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,15 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+
+  let uli = document.createElement("ul");
+  for (let friend of friends){
+    if(friend){
+      let li = document.createElement("li");
+      li.innerHTML  = ` ${friend.firstName} ${friend.lastName}`;
+      uli.appendChild(li);
+    }
+  }
+
+return uli;
 }
